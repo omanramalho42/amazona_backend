948cfdae06d9d843b5f46c5c5184594350cb18f3
oman <omanapple42@hotmail.com>
2023-01-04 14:46:41 -0300

    creating product screen route and modify value stock in pspecific product

--- server.js
+++ server.js
@@ -10,6 +10,20 @@ app.get("/api/products", (req, res) => {
   res.status(200).send(data.products);
 });
 
+app.get("/api/products/slug/:slug", async (req, res) => {
+
+  const product = data.products.find(
+    product => product.slug === req.params.slug
+  );
+  
+  if(product) {
+    res.status(200).send(product);
+  } else {
+    res.status(404).send({ message: 'Produto não encontrado' });
+  }
+
+});
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log("Server is Running on port: http://localhost:"+port);

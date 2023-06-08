8e2fe8e2e329df28f64b88d52eb795db2c642461
oman <omanapple42@hotmail.com>
2023-06-08 12:21:00 -0300

    allow cors and headers

--- server.js
+++ server.js
@@ -24,6 +24,11 @@ mongoose.connect(process.env.MONGODB_URI)
 const app = express();
 
 app.use(cors());
+app.use(function (req,res,next) {
+  res.header("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT, DELETE")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
